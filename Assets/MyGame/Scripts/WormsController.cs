a5a9e1e1730cc8b48be16f85f782cc9003a056c3
unknown <eva.schwarzl@htl-salzburg.ac.at>
2020-02-05 09:55:43 +0100

    added the ability to shoot ammunition

--- Assets/MyGame/Scripts/WormsController.cs
+++ Assets/MyGame/Scripts/WormsController.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class WormsController : MonoBehaviour
 {
+    public Rigidbody rbAmmo;
     public Rigidbody rb;
     public float jumpForce;
     public float moveForce;
+    public float AmmoForce;
 
 
     void Update()
@@ -34,6 +36,20 @@ public class WormsController : MonoBehaviour
 
             rb.AddForce(keya);
             Debug.Log("Got Key A");
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Rigidbody clone;
+           
+            Vector3 fix = new Vector3(0.5f, 0.5f, 0f);
+            clone = Instantiate(rbAmmo,transform.position + fix, transform.rotation);
+            clone.velocity = transform.TransformDirection(AmmoForce, 0f, 0f);
+
+            Vector3 keyp = new Vector3(AmmoForce, 0, 0);
+            rbAmmo.AddForce(keyp);
+
+
+            
         }
     }
 }
